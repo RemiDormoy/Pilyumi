8fbb3b21cb3adc0700bd22aafd98ba6d45819d1f
Remi Dormoy <rdormoy@octo.com>
2019-03-30 15:41:28 +0100

    card rotation

--- app/src/main/java/pilyumi/octo/pilyumi/QuizzActivity.kt
+++ app/src/main/java/pilyumi/octo/pilyumi/QuizzActivity.kt
@@ -1,13 +1,86 @@
 package pilyumi.octo.pilyumi
 
+import android.animation.ValueAnimator
+import android.content.res.Resources
 import android.os.Bundle
+import android.os.Handler
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
+import android.util.TypedValue
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import kotlinx.android.synthetic.main.activity_quizz.*
+import kotlinx.android.synthetic.main.first_question.*
+import kotlinx.android.synthetic.main.second_question.*
+import kotlinx.android.synthetic.main.third_question.*
 
 class QuizzActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizz)
+        firstQuestionButton.setOnClickListener {
+            firstSuccessView.animate().alpha(1f).withEndAction {
+                firstLottieView.playAnimation()
+                Handler().postDelayed({
+                    moveTo2()
+                }, 1000)
+            }
+        }
+        secondQuestionButton.setOnClickListener {
+            moveTo3()
+        }
+        thirdQuestionButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun moveTo2() {
+        val animator = ValueAnimator.ofInt(0, 1000)
+        animator.addUpdateListener {
+            val value = it.animatedValue as Int
+            cardFirstQuestion.translationX = -value.toFloat()
+
+            cardSecondQuestion.translationX = ((1000f - value) / 1000f) * 50.toDp(resources)
+            cardSecondQuestion.translationY = ((1000f - value) / 1000f) * 10.toDp(resources)
+            cardSecondQuestion.rotation = 5 * (1000f - value) / 1000f
+
+            cardThirdQuestion.translationX = ((1000f - value) / 1000f) * 70.toDp(resources) + (value / 1000f) * 50.toDp(resources)
+            cardThirdQuestion.translationY = ((1000f - value) / 1000f) * 30.toDp(resources) + (value / 1000f) * 10.toDp(resources)
+            cardThirdQuestion.rotation = 5 * (1000f - value) / 1000f + (value / 1000f) * 5
+        }
+        animator.duration = 500
+        animator.interpolator = AccelerateDecelerateInterpolator()
+        animator.start()
+    }
+
+    private fun moveTo3() {
+        val animator = ValueAnimator.ofInt(0, 1000)
+        animator.addUpdateListener {
+            val value = it.animatedValue as Int
+            cardSecondQuestion.translationX = -value.toFloat()
+
+            cardThirdQuestion.translationX = ((1000f - value) / 1000f) * 50.toDp(resources)
+            cardThirdQuestion.translationY = ((1000f - value) / 1000f) * 10.toDp(resources)
+            cardThirdQuestion.rotation = 5 * (1000f - value) / 1000f
+        }
+        animator.duration = 500
+        animator.interpolator = AccelerateDecelerateInterpolator()
+        animator.start()
     }
 
 }
+
+fun Int.toDp(resources: Resources) = TypedValue.applyDimension(
+    TypedValue.COMPLEX_UNIT_DIP,
+    this.toFloat(),
+    resources.getDisplayMetrics()
+)
+
+fun Float.toDp(resources: Resources) = TypedValue.applyDimension(
+    TypedValue.COMPLEX_UNIT_DIP,
+    this,
+    resources.getDisplayMetrics()
+)
--- app/src/main/java/pilyumi/octo/pilyumi/ViewPagerFragment.kt
+++ app/src/main/java/pilyumi/octo/pilyumi/ViewPagerFragment.kt
@@ -1,10 +1,12 @@
 package pilyumi.octo.pilyumi
 
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
+import kotlinx.android.synthetic.main.fragment_did_you_know.view.*
 import kotlinx.android.synthetic.main.fragment_feelings.view.*
 
 
@@ -20,30 +22,38 @@ class ViewPagerFragment(private val position: Int) : Fragment() {
         }
         val inflate = inflater.inflate(layout, container, false)
         if (position == 2) {
-            val lottie = inflate.lottieViewFeelings
-            listOf(
-                inflate.coolImageView,
-                inflate.cryingImageView8,
-                inflate.sickImageView,
-                inflate.spmImageView
-            ).forEach {
-                it.setOnClickListener {
-                    listOf(
-                        inflate.coolImageView,
-                        inflate.sickImageView,
-                        inflate.cryingImageView8,
-                        inflate.spmImageView,
-                        inflate.textView9,
-                        inflate.textView10,
-                        inflate.textView11,
-                        inflate.textView12).forEach {
-                        it.animate().alpha(0f).setDuration(300).withEndAction {
-                            lottie.playAnimation()
-                        }.start()
-                    }
-                }
+            configureForFeelings(inflate)
+        } else if (position == 0) {
+            inflate.buttonquizz.setOnClickListener {
+                startActivity(Intent(requireContext(), QuizzActivity::class.java))
             }
         }
         return inflate
+    }
+
+    private fun configureForFeelings(inflate: View) {
+        val lottie = inflate.lottieViewFeelings
+        listOf(
+            inflate.coolImageView,
+            inflate.cryingImageView8,
+            inflate.sickImageView,
+            inflate.spmImageView
+        ).forEach {
+            it.setOnClickListener {
+                listOf(
+                    inflate.coolImageView,
+                    inflate.sickImageView,
+                    inflate.cryingImageView8,
+                    inflate.spmImageView,
+                    inflate.textView9,
+                    inflate.textView10,
+                    inflate.textView11,
+                    inflate.textView12).forEach {
+                    it.animate().alpha(0f).setDuration(300).withEndAction {
+                        lottie.playAnimation()
+                    }.start()
+                }
+            }
+        }
     }
 }
